a0556fc6be55aaa5939a8cd25f29ea7394543fc3
Eric Hainer <eric.hainer@gmail.com>
2016-03-22 17:35:24 -0600

    Throw exception on error validating nonce

--- vendor/magento/module-integration/Model/Oauth/Nonce/Generator.php
+++ vendor/magento/module-integration/Model/Oauth/Nonce/Generator.php
@@ -98,7 +98,7 @@ class Generator implements NonceGeneratorInterface
         } catch (\Magento\Framework\Oauth\Exception $exception) {
             throw $exception;
         } catch (\Exception $exception) {
-            throw new \Magento\Framework\Oauth\Exception(__($exception->getMessage()));
+            throw new \Magento\Framework\Oauth\Exception(__("{$timestamp} - {time()}"));
             throw new \Magento\Framework\Oauth\Exception(__('An error occurred validating the nonce'));
         }
     }
