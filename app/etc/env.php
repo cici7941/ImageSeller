5af63f311e10a376e847b14c42a7f4282e8826fd
Eric Hainer <eric.hainer@gmail.com>
2016-03-22 11:57:26 -0600

    Initial Commit

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 22 Mar 2016 04:47:41 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '34c6eb4b2be3eb3d858b479a68c603f9',
+  ),
+  'session' => 
+  array (
+    'save' => 'db',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'imageseller_development',
+        'username' => 'root',
+        'password' => 'ui0p0iu',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
